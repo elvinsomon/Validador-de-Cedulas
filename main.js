f956c483aba396febfc1e351fa326810d502127c
elvinsomon <elvinsomon2001@gmail.com>
2020-06-23 00:20:24 -0400

    Ultimo commit

--- main.js
+++ main.js
@@ -1,49 +1,60 @@
 //1.
-let cedula = prompt('Favor ingrese su cédula');
+//let cedula = prompt('Favor ingrese su cédula');
 
-//let cedula = '402-1474721-0';
+//let cedula = 'let cedula = '402-1474721-0';';
 
-//2.
-cedula = cedula.replace(/-/g, '');
-let ultNum = cedula[10];
+let formulario = document.getElementById('form');
+let btn = document.getElementById('boton');
 
-//3.
-cedula = cedula.substring(0, 10);
+btn.addEventListener('click', validar(), true);
 
-//4.
-cedula = cedula.split('');
+function validar() {
+	//2.
+	let cedula = document.getElementById('textbox').value;
+	cedula = cedula.toString();
+	cedula = cedula.replace(/-/g, '');
+	let ultNum = cedula[10];
 
-for (let x = 0; x < cedula.length; x++) {
-	if (x % 2 === 0) {
-		cedula[x] = parseInt(cedula[x]) * 1;
-	} else {
-		cedula[x] = parseInt(cedula[x]) * 2;
+	//3.
+	cedula = cedula.substring(0, 10);
+
+	//4.
+	cedula = cedula.split('');
+
+	for (let x = 0; x < cedula.length; x++) {
+		if (x % 2 === 0) {
+			cedula[x] = parseInt(cedula[x]) * 1;
+		} else {
+			cedula[x] = parseInt(cedula[x]) * 2;
+		}
 	}
-}
 
-//5.
+	//5.
 
-let total = 0;
+	let total = 0;
 
-for (let x = 0; x < cedula.length; x++) {
-	if (cedula[x] > 9) {
-		cedula[x] =
-			parseInt(cedula[x].toString().split('')[0]) +
-			parseInt(cedula[x].toString().split('')[1]);
-	} else {
-		cedula[x] = cedula[x];
+	for (let x = 0; x < cedula.length; x++) {
+		if (cedula[x] > 9) {
+			cedula[x] =
+				parseInt(cedula[x].toString().split('')[0]) +
+				parseInt(cedula[x].toString().split('')[1]);
+		} else {
+			cedula[x] = cedula[x];
+		}
+		total = total + parseInt(cedula[x]);
 	}
-	total = total + parseInt(cedula[x]);
+
+	//6.
+	let result = 0;
+	let decenaSuper = total / 10;
+	decenaSuper = Math.ceil(decenaSuper);
+	decenaSuper = decenaSuper * 10;
+	result = decenaSuper - total;
+
+	let valido =
+		result == ultNum
+			? alert(':D ¡Cédula Válida!')
+			: alert(' :( Cédula Inválida');
+
+	return valido;
 }
-
-//6.
-let result = 0;
-let decenaSuper = total / 10;
-decenaSuper = Math.ceil(decenaSuper);
-decenaSuper = decenaSuper * 10;
-result = decenaSuper - total;
-
-console.log(result);
-
-let valido =
-	result == ultNum ? alert(':D ¡Cédula Válida!') : alert(' :( Cédula Inválida');
